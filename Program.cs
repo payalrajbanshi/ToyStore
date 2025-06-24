1df4219a31da32793ef1866841b5bdacc35cb118
Payal Rajbanshi <payalrajbansi8@gmail.com>
2025-06-24 21:04:38 +0545

    Updated feature

--- Program.cs
+++ Program.cs
@@ -1,33 +1,3 @@
-// using Microsoft.EntityFrameworkCore;
-// using ToyStore.Data;
-// using ToyStore.Services;
-
-
-// var builder = WebApplication.CreateBuilder(args);
-// builder.Services.AddControllersWithViews();
-// // 🔗 Replace with your own connection string
-// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-// builder.Services.AddDbContext<AppDbContext>(options =>
-//     options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32))));
-//     builder.Services.AddHttpContextAccessor();
-// builder.Services.AddSession();             
-// builder.Services.AddScoped<CartService>();
-// var app = builder.Build();
-// app.UseStaticFiles();
-// app.UseRouting();
-// app.UseSession();
-// //app.MapControllerRoute(name: "default", pattern: "{controller=Product}/{action=Index}/{id?}");
-// app.MapControllerRoute(
-//     name: "default",
-//     pattern: "{controller=Product}/{action=Index}/{id?}");
-
-// using (var scope = app.Services.CreateScope())
-// {
-//     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-//     SeedData.Initialize(context);
-// }
-
-// app.Run();
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ToyStore.Data;
@@ -37,18 +7,11 @@ using ToyStore.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
-// Connection string from appsettings.json
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-// Add DbContext with MySQL
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 32))));
-
-// Add Identity services
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
 {
-    // You can customize password options here if needed
     options.Password.RequireDigit = false;
     options.Password.RequiredLength = 6;
     options.Password.RequireNonAlphanumeric = false;
@@ -63,18 +26,13 @@ builder.Services.AddSession();
 builder.Services.AddScoped<CartService>();
 
 var app = builder.Build();
-
-// Middleware pipeline
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseSession();
 
-app.UseAuthentication();  // <-- Add Authentication middleware
-app.UseAuthorization();   // <-- Add Authorization middleware
-
-// Seed roles and admin user asynchronously
+app.UseAuthentication();  
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
@@ -91,7 +49,4 @@ app.MapControllerRoute(
     context.Response.Redirect("/Account/Register");
     return Task.CompletedTask;
 });
-
-
-
 app.Run();
